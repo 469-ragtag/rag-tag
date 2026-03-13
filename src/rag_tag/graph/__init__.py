d3def52a6a6dc6c4b7c61d1164f260cc24f4e8ec
Ishaan6233 <ishhaan.m@gmail.com>
2026-03-13 13:30:46 -0600

    ruff fixtures

--- src/rag_tag/graph/__init__.py
+++ src/rag_tag/graph/__init__.py
@@ -7,13 +7,21 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from rag_tag.graph.runtime import GraphRuntime, register_backend
 
-__all__ = ["GraphRuntime", "register_backend"]
+__all__ = ["GraphRuntime", "register_backend", "wrap_networkx_graph"]
 
 
 def __getattr__(name: str):
     if name in __all__:
-        from rag_tag.graph.runtime import GraphRuntime, register_backend
+        from rag_tag.graph.runtime import (
+            GraphRuntime,
+            register_backend,
+            wrap_networkx_graph,
+        )
 
-        exports = {"GraphRuntime": GraphRuntime, "register_backend": register_backend}
+        exports = {
+            "GraphRuntime": GraphRuntime,
+            "register_backend": register_backend,
+            "wrap_networkx_graph": wrap_networkx_graph,
+        }
         return exports[name]
     raise AttributeError(name)
